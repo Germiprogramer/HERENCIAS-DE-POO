e7c1bdb668cb35466bd067a9ec5e1f71132c1adc
Germiprogramer <llorentemunozgerman@gmail.com>
2022-03-19 19:09:36 +0100

    creada la clase punto2D

---
+++ classes/ejercicio1.py
@@ -0,0 +1,18 @@
+class Punto2D:
+    def __init__(self, x, y):
+        self.x = x
+        self.y = y
+    
+    def get_x(self):
+        return self.x
+
+    def set_x(self, a):
+        self.x = a
+        
+    def get_y(self):
+        return self.y
+
+    def set_y(self, a):
+        self.y = a
+
+
